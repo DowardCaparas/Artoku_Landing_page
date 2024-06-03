000e559d6ab61a33b72f2a17ca7854c468083e14
Dounhuward B. Caparas <caparasdounhuward@gmail.com>
2024-06-03 12:05:29 +0800

    adding file index.ts in public folder

--- public/index.ts
+++ public/index.ts
@@ -1,3 +1,30 @@
-import {check} from './check.svg'
 
-export {check}
+import check from './check.svg'
+import chevrondown from './chevrondown.svg'
+import chevronleft from './chevronleft.svg'
+import chevronright from './chevronright.svg'
+import chevronup from './chevronup.svg'
+import linkedin from './linkedIn.svg'
+import message from './message.svg'
+import  pie from './pie.svg'
+import question from './question.svg'
+import rightarrow from './rightarrow.svg'
+import twitter from './twitter.svg'
+import user from './user.svg'
+import wallet from './wallet.svg'
+
+export {
+    check,
+    chevrondown,
+    chevronleft,
+    chevronright,
+    chevronup,
+    linkedin,
+    message,
+    pie,
+    question,
+    rightarrow,
+    twitter,
+    user,
+    wallet
+}
